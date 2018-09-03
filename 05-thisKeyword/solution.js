24c596c93897546b3731aeddc1c8f51709937982
sampalm <samuelpalmeira@outlook.com>
2018-09-03 17:13:48 -0300

    adds pratice 05 to the list

---
+++ 05-thisKeyword/solution.js
@@ -0,0 +1,7 @@
+/*
+Problem:  Explains whats the "this" keyword and how are they used and code out an example.
+R: Is general the "this" keyword reffers to the object that its directly inside of.
+*/
+
+// 1- Solution
+console.log(this) // returns the global window object
