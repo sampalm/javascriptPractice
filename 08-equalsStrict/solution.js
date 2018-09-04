858d7723cd7d8ef0bc9d249e0fa9845c3022c702
sampalm <samuelpalmeira@outlook.com>
2018-09-04 18:13:59 -0300

    adds pratice 08 to the list

---
+++ 08-equalsStrict/solution.js
@@ -0,0 +1,10 @@
+/*
+Problem:  Whats the difference between the comparison operators == and === ?
+*/
+
+// Equals
+if ("0" == 0) console.log(true) // Returns true because only compare the values
+
+
+// Strict Equals
+if ("0" === 0) console.log(true) // Returns false because it compares the values and the types
