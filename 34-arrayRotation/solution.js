8d70c01735645e60d65c5c4002c6859e72566a1c
sampalm <samuelpalmeira@outlook.com>
2018-09-21 20:16:47 -0300

    adds Array Rotation challenge

---
+++ 34-arrayRotation/solution.js
@@ -0,0 +1,7 @@
+/*
+Problem: You're a given two arrays without duplicated elements, write a function which takes those two arrays and returns true if one of them is a rotation of the other, which means that both conteins the same elements in the same order but start in a different index.
+*/
+
+// Code Solution
+var arr1 = [1,2,3,4,5,6,7];
+var arr2 = [4,5,6,7,1,2,3];
