2d223097de53a31375946dcc7bc434a5ebe83887
sampalm <samuelpalmeira@outlook.com>
2018-09-24 20:54:55 -0300

    adds Non-Repeating Characters challenge

---
+++ 35-nonRepeatingChar/solution.js
@@ -0,0 +1,14 @@
+/*
+Problem: Implements a function that takes a string and return the character that appears only once or null if none.
+*/
+
+// Code Solution
+var str1 = "abcd"
+var str2 = "aabbc"
+var str3 = "aabbcd"
+var str4 = "aabbdd"
+
+
+
+
+
