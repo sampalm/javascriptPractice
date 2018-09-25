ea0b0e2e5dad2d6dfeeb9c0c92c0d239fa68e5b7
sampalm <samuelpalmeira@outlook.com>
2018-09-24 21:43:10 -0300

    adds Non-Repeating Characters solution #1

--- 35-nonRepeatingChar/solution.js
+++ 35-nonRepeatingChar/solution.js
@@ -8,7 +8,19 @@ var str2 = "aabbc"
 var str3 = "aabbcd"
 var str4 = "aabbdd"
 
+function nonRepeating(string) {
+    for(i=0;i<string.length;i++) {
+      if (string.charAt(i) == string.charAt(i-1) || string.charAt(i) == string.charAt(i+1)) {
+        continue
+      }
+      return string.charAt(i)
+    }
+}
 
+console.log(nonRepeating(str1));
+console.log(nonRepeating(str2));
+console.log(nonRepeating(str3));
+console.log(nonRepeating(str4));
 
 
 
