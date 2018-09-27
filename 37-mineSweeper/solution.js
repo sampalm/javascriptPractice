6c9f909298de737c42c9648bb7f2d94373e6d5b9
sampalm <samuelpalmeira@outlook.com>
2018-09-26 22:15:24 -0300

    adds Numbers in Minesweeper challenge

---
+++ 37-mineSweeper/solution.js
@@ -0,0 +1,15 @@
+/*
+* Problem: Writes a function that takes three arguments:
+* - The first of thoses is going to be "bombs"  which shows a list of bomb locations
+* - The second one will be the numbers of rows
+* - The third one will be the numbers of columns
+* - Assume that are no duplicates
+*/
+
+//  Code example
+/*
+    mineSweeper([[0,0], [0,1]], 3, 4)
+    [[-1, -1, 1, 0],
+     [2, 2, 1, 0],
+     [0, 0, 0, 0]]
+*/
