63085c7edee1279cef228095d90bacaed66723fe
sampalm <samuelpalmeira@outlook.com>
2018-10-01 23:40:34 -0300

    adds Linked List challenge

---
+++ 40-linkedList/solution.js
@@ -0,0 +1,5 @@
+/**
+ *  Problem: Implementing a function called ends from end which takes two arguments "head" which is the given * length list and "n" which is a positive integer. You want to be able to find the end node in this linked * * list counting from the end.
+ *  head = [5 > 4 > 3 > 2 > 1 ]
+ *  nth_from_end(head, 3) -> 3
+ */
