d4be45d807864eb26c482a17a12fbd64d7e8808d
sampalm <samuelpalmeira@outlook.com>
2018-10-01 23:32:33 -0300

    adds Rotating Array challenge

---
+++ 39-rotatingArray/solution.js
@@ -0,0 +1,16 @@
+/**
+ *  Problem: Writing a function that takes this array and rotates it by 90 degrees clockwise.
+ */
+
+ // Code Example
+ /*
+ [[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]]
+==>
+rotate(givenArray, n) 
+==>
+[[7, 4, 1],
+[8, 5, 2],
+[9, 6, 3]]
+*/
